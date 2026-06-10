1c3870b1c10c4b851e5d3c6600719f3ff7e92a6d
artem.yanko <artem.yanko@mserverone.com>
2026-06-10 14:07:01 +0000

    chore: cleanup old update drone motion

--- homework_07/src/core/MissionProcessor.cpp
+++ homework_07/src/core/MissionProcessor.cpp
@@ -7,8 +7,6 @@
 #include <cmath>
 #include <fstream>
 
-static const double SLOW_TURN_THRESHOLD_FACTOR = 3.0;
-
 static bool writeSimulationJson(const std::vector<SimStep>& steps) {
   std::ofstream outputFile("simulation.json");
   if (!outputFile) {
@@ -55,161 +53,8 @@ static double calculateDroneAcceleration(double attackSpeed, double acceleration
   return attackSpeed * attackSpeed / (2.0 * accelerationPath);
 }
 
-static void moveDroneToStop(Coord& position, double direction, double distance) {
-  position.x += cos(direction) * distance;
-  position.y += sin(direction) * distance;
-}
-
-static bool moveDroneToPoint(Coord& position, const Coord& dest, double maxDistance){
-  Coord delta = dest - position;
-  double distance = length(delta);
-  if (distance == 0.0) {
-    return true;
-  }
-
-  if (maxDistance >= distance) {
-      position = dest;
-  return true;
-  }
-
-  position += normalize(delta) * maxDistance;
-  return false;
-}
-
 static bool isInsideRadius(const Coord& point, const Coord& center, double radius) {
   return distanceBetween(point, center) <= radius;
-}
-
-static bool updateDroneMotion(Coord& dronePosition, DroneMotionState& droneMotion, const Coord& goal, double simTimeStep, double attackSpeed, double acceleration, double angularSpeed, double turnThreshold) {
-
-  Coord deltaToGoal = goal - dronePosition;
-  double desiredDir = droneMotion.currentDir;
-
-  if (deltaToGoal.x != 0.0 || deltaToGoal.y != 0.0) {
-    desiredDir = atan2(deltaToGoal.y, deltaToGoal.x);
-  }
-
-  if (droneMotion.phase != TURNING) {
-    double deltaAngle = fabs(calculateAngleDifference(droneMotion.currentDir, desiredDir));
-    double slowTurnThreshold = turnThreshold * SLOW_TURN_THRESHOLD_FACTOR;
-    bool shouldStopForTurn = deltaAngle > turnThreshold;
-    if (droneMotion.phase == MOVING) {
-      shouldStopForTurn = deltaAngle > slowTurnThreshold;
-    }
-
-    if (shouldStopForTurn) {
-      droneMotion.turnTargetDir = desiredDir;
-      if (droneMotion.currentSpeed > 0.0) {
-        droneMotion.phase = DECELERATING;
-      } else {
-        droneMotion.phase = TURNING;
-        droneMotion.turnRemainingTime = deltaAngle / angularSpeed;
-      }
-    } else {
-      if (deltaAngle <= turnThreshold) {
-        droneMotion.currentDir = desiredDir;
-      }
-      if (droneMotion.phase == STOPPED) {
-        droneMotion.phase = ACCELERATING;
-      }
-    }
-  }
-
-  bool reachedGoal = false;
-
-  if (droneMotion.phase == DECELERATING) {
-    double oldSpeed = droneMotion.currentSpeed;
-    droneMotion.currentSpeed -= acceleration * simTimeStep;
-    if (droneMotion.currentSpeed < 0.0) {
-      droneMotion.currentSpeed = 0.0;
-    }
-
-    double moveDistance = (oldSpeed + droneMotion.currentSpeed) * simTimeStep / 2.0;
-    moveDroneToStop(dronePosition, droneMotion.currentDir, moveDistance);
-    if (droneMotion.currentSpeed == 0.0) {
-      double turnAngle = fabs(calculateAngleDifference(droneMotion.currentDir, desiredDir));
-      droneMotion.turnTargetDir = desiredDir;
-      if (turnAngle > 0.0) {
-        droneMotion.phase = TURNING;
-        droneMotion.turnRemainingTime = turnAngle / angularSpeed;
-      } else {
-        droneMotion.phase = ACCELERATING;
-        droneMotion.turnRemainingTime = 0.0;
-      }
-    }
-  } else if (droneMotion.phase == TURNING) {
-    double angleLeft = calculateAngleDifference(droneMotion.currentDir, droneMotion.turnTargetDir);
-    double turnStep = angularSpeed * simTimeStep;
-
-    if (fabs(angleLeft) <= turnStep) {
-      droneMotion.currentDir = droneMotion.turnTargetDir;
-      droneMotion.turnRemainingTime = 0.0;
-      droneMotion.phase = ACCELERATING;
-    } else {
-      if (angleLeft > 0.0) {
-        droneMotion.currentDir += turnStep;
-        if (droneMotion.currentDir > M_PI) {
-          droneMotion.currentDir -= 2.0 * M_PI;
-        }
-      } else {
-        droneMotion.currentDir -= turnStep;
-        if (droneMotion.currentDir < -M_PI) {
-          droneMotion.currentDir += 2.0 * M_PI;
-        }
-      }
-      droneMotion.turnRemainingTime = (fabs(angleLeft) - turnStep) / angularSpeed;
-    }
-  } else if (droneMotion.phase == ACCELERATING) {
-
-    double oldSpeed = droneMotion.currentSpeed;
-    droneMotion.currentSpeed += acceleration * simTimeStep;
-    if (droneMotion.currentSpeed > attackSpeed) {
-      droneMotion.currentSpeed = attackSpeed;
-    }
-
-    double moveDistance = (oldSpeed + droneMotion.currentSpeed) * simTimeStep / 2.0;
-    reachedGoal = moveDroneToPoint(dronePosition, goal, moveDistance);
-
-    if (droneMotion.currentSpeed >= attackSpeed) {
-      droneMotion.phase = MOVING;
-    }
-  } else if (droneMotion.phase == MOVING) {
-    double angleLeft = calculateAngleDifference(droneMotion.currentDir, desiredDir);
-    double turnStep = angularSpeed * simTimeStep;
-    double slowTurnThreshold = turnThreshold * SLOW_TURN_THRESHOLD_FACTOR;
-    if (fabs(angleLeft) <= slowTurnThreshold) {
-      if (fabs(angleLeft) <= turnStep) {
-        droneMotion.currentDir = desiredDir;
-      } else if (angleLeft > 0.0) {
-        droneMotion.currentDir += turnStep;
-        if (droneMotion.currentDir > M_PI) {
-          droneMotion.currentDir -= 2.0 * M_PI;
-        }
-      } else {
-        droneMotion.currentDir -= turnStep;
-        if (droneMotion.currentDir < -M_PI) {
-          droneMotion.currentDir += 2.0 * M_PI;
-        }
-      }
-    }
-
-    droneMotion.currentSpeed = attackSpeed;
-    reachedGoal = moveDroneToPoint(dronePosition, goal, droneMotion.currentSpeed * simTimeStep);
-  } else if (droneMotion.phase == STOPPED) {
-    droneMotion.currentSpeed = 0.0;
-
-    double deltaAngle = fabs(calculateAngleDifference(droneMotion.currentDir, desiredDir));
-    if (deltaAngle > turnThreshold) {
-      droneMotion.phase = TURNING;
-      droneMotion.turnTargetDir = desiredDir;
-      droneMotion.turnRemainingTime = deltaAngle / angularSpeed;
-    } else {
-      droneMotion.currentDir = desiredDir;
-      droneMotion.phase = ACCELERATING;
-    }
-  }
-
-  return reachedGoal;
 }
 
 MissionProcessor::~MissionProcessor() = default;
@@ -386,7 +231,6 @@ BallisticsResult MissionProcessor::step() {
       droneState = std::move(next);
   }
 
-    // updateDroneMotion(dronePosition, droneMotion, goal, config.simTimeStep, config.attackSpeed, acceleration, config.angularSpeed, config.turnThreshold);
     DEBUG("Drone position: (" << dronePosition.x << "," << dronePosition.y << ")"
         << ", dir=" << droneMotion.currentDir
         << ", speed=" << droneMotion.currentSpeed
